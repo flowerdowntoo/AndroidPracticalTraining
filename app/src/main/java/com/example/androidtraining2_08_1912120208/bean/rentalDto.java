c6f79d7a69b945bc08f993a00bd23198a9db179f
hxb <hxb@2692750313>
2022-06-27 20:29:11 +0800

    rentalDto加入一个Appointment（后端修复了首页预约数据加载的逻辑bug 现在会加载审核通过并且可以预约的车辆 但是时间问题待解决）

--- app/src/main/java/com/example/androidtraining2_08_1912120208/bean/rentalDto.java
+++ app/src/main/java/com/example/androidtraining2_08_1912120208/bean/rentalDto.java
@@ -6,6 +6,15 @@ import com.chad.library.adapter.base.entity.MultiItemEntity;
 public class rentalDto extends Rental  implements MultiItemEntity {
     private User user;
     private Car car;
+    private Appointment appointment;
+
+    public Appointment getAppointment() {
+        return appointment;
+    }
+
+    public void setAppointment(Appointment appointment) {
+        this.appointment = appointment;
+    }
 
     public User getUser() {
         return user;
